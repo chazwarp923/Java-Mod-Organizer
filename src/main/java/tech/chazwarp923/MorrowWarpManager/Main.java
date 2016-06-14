6482dea6f9a3c0942b1e5b9608c17658bbbb0b38
Chaz <chazwarp923@icloud.com>
2016-06-14 16:54:14 -0400

    Now successfully downloads mods through nexus links, so long as file us under nexus' 2mb limit

--- src/main/java/tech/chazwarp923/MorrowWarpManager/Main.java
+++ src/main/java/tech/chazwarp923/MorrowWarpManager/Main.java
@@ -1,11 +1,13 @@
 package tech.chazwarp923.MorrowWarpManager;
 
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
 import java.net.URL;
+
+import org.apache.commons.io.FileUtils;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 
 /**
  * Created by Chazwarp923 on 6/12/16.
@@ -27,62 +29,24 @@ public class Main {
     }
     
     private static void handleNexusLink (String nxm) throws IOException {
-    	String url = nxm;
-        url = url.replace("nxm://", "http://www.nexusmods.com/");
-        System.out.println(url);
-        URL nexusLink = new URL(url);
-		
-        //Begin code borrowed from http://www.codejava.net/java-se/networking/use-httpurlconnection-to-download-file-from-an-http-url
-        HttpURLConnection httpConn = (HttpURLConnection) nexusLink.openConnection();
-        int responseCode = httpConn.getResponseCode();
-        if (responseCode == HttpURLConnection.HTTP_OK) {
-            String fileName = "";
-            String disposition = httpConn.getHeaderField("Content-Disposition");
-            String contentType = httpConn.getContentType();
-            int contentLength = httpConn.getContentLength();
- 
-            if (disposition != null) {
-                // extracts file name from header field
-                int index = disposition.indexOf("filename=");
-                if (index > 0) {
-                    fileName = disposition.substring(index + 10,
-                            disposition.length() - 1);
-                }
-            } else {
-                // extracts file name from URL
-                fileName = url.substring(url.lastIndexOf("/") + 1,
-                		url.length());
-            }
- 
-            System.out.println("Content-Type = " + contentType);
-            System.out.println("Content-Disposition = " + disposition);
-            System.out.println("Content-Length = " + contentLength);
-            System.out.println("fileName = " + fileName);
- 
-            // opens input stream from the HTTP connection
-            InputStream inputStream = httpConn.getInputStream();
-            String saveDir = System.getProperty("java.io.tmpdir") + "/MorrowWarpManager";
-            String saveFilePath = saveDir + File.separator + fileName;
-             
-            // opens an output stream to save into file
-            File tmpFile = new File(saveDir);
-            tmpFile.mkdirs();
-            FileOutputStream outputStream = new FileOutputStream(saveFilePath);
- 
-            int bytesRead = -1;
-            byte[] buffer = new byte[4096];
-            while ((bytesRead = inputStream.read(buffer)) != -1) {
-                outputStream.write(buffer, 0, bytesRead);
-            }
- 
-            outputStream.close();
-            inputStream.close();
- 
-            System.out.println("File downloaded");
-        } else {
-            System.out.println("No file to download. Server replied HTTP code: " + responseCode);
-        }
-        httpConn.disconnect();
+    	String game = nxm.substring(6, nxm.indexOf("/mods")).toLowerCase();
+    	String modId = nxm.substring(6 + game.length() + 6, nxm.indexOf("/files"));
+    	String fileId = nxm.substring(6 + game.length() + 6 + modId.length() + 7, nxm.length());
+    	String url = "http://www.nexusmods.com/" + game + "/download/" + fileId;
+    	
+    	Document doc = Jsoup.connect(url).get();
+        Element links = doc.select("script[language][type]").first();
+        URL downloadLink = new URL(links.html().substring(links.html().indexOf('"') + 1, links.html().length() - 2));
+    	
+    	System.out.println("Nexus Link: " + nxm);
+    	System.out.println("Game: " + game);
+    	System.out.println("ModID: " + modId);
+    	System.out.println("FileID: " + fileId);
+    	System.out.println("Download Page: " + url);      
+        System.out.println("Download Link: " + downloadLink);
+        
+        String saveDir = System.getProperty("java.io.tmpdir") + "/MorrowWarpManager";
+        String saveFilePath = saveDir + File.separator + downloadLink.toString().substring(downloadLink.toString().lastIndexOf("/") + 1, downloadLink.toString().lastIndexOf("?"));;
+        FileUtils.copyURLToFile(downloadLink, new File(saveFilePath));	
     }
-    //End borrowed code
 }
